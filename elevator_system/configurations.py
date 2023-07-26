b8c9336ecfbf209af1e1632c19a74aec17beb344
Aman <aman@Amans-MacBook-Air-2.local>
2023-07-26 13:58:02 +0530

    Basic Setup

---
+++ elevator_system/configurations.py
@@ -0,0 +1,6 @@
+SYSTEM_KEY = "django-insecure-igq3h$!l1xmxh!g@hq&aw3c3vqdaj^nt^y(y*h_g3s3)jg-8sp"
+DATABASE_NAME = 'elevator'
+DATABASE_USER = 'root'
+DATABASE_PASSWORD = 'Aman*0112'
+DATABASE_HOST = '127.0.0.1'
+DATABASE_PORT = '3306'
